2c52e61bf6f0e840e4fad4ff223661f4a08aa45b
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-05-23 12:20:37 +0700

    file_player: migrate initial node bootstrap to ROS2

--- file_player/src/mainwindow.cpp
+++ file_player/src/mainwindow.cpp
@@ -3,19 +3,19 @@
 
 using namespace std;
 
-MainWindow::MainWindow(QWidget *parent) :
-  QMainWindow(parent),
-  ui_(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      my_ros_(new ROSThread(this, &mutex)),
+      ui_(new Ui::MainWindow),
+      play_flag_(false),
+      pause_flag_(false),
+      loop_flag_(false),
+      stop_skip_flag_(true),
+      slider_value_(0),
+      slider_checker_(false)
 {
-  my_ros_ = new ROSThread(this, &mutex);
   ui_->setupUi(this);
   my_ros_->start();
-
-  slider_checker_ = false;
-  play_flag_ = false;
-  pause_flag_ = false;
-  loop_flag_ = false;
-  stop_skip_flag_ = true;
 
   connect(my_ros_, SIGNAL(StampShow(quint64)), this, SLOT(SetStamp(quint64)));
   connect(my_ros_, SIGNAL(StartSignal()), this, SLOT(Play()));
@@ -26,124 +26,108 @@ MainWindow::MainWindow(QWidget *parent) :
   connect(ui_->pushButton_3, SIGNAL(pressed()), this, SLOT(Pause()));
 
   connect(ui_->doubleSpinBox, SIGNAL(valueChanged(double)), this, SLOT(PlaySpeedChange(double)));
-  ui_->doubleSpinBox->setRange(0.01,20.0);
+  ui_->doubleSpinBox->setRange(0.01, 20.0);
   ui_->doubleSpinBox->setValue(1.0);
   ui_->doubleSpinBox->setSingleStep(0.01);
-  connect(ui_->checkBox, SIGNAL(stateChanged(int)), this, SLOT (LoopFlagChange(int)));
-  if(loop_flag_ == true){
-    ui_->checkBox->setCheckState(Qt::Checked);
-  }else{
-    ui_->checkBox->setCheckState(Qt::Unchecked);
-  }
-  connect(ui_->checkBox_2, SIGNAL(stateChanged(int)), this, SLOT (StopSkipFlagChange(int)));
-  if(stop_skip_flag_ == true){
-    ui_->checkBox_2->setCheckState(Qt::Checked);
-  }else{
-    ui_->checkBox_2->setCheckState(Qt::Unchecked);
-  }
-  connect(ui_->checkBox_3, SIGNAL(stateChanged(int)), this, SLOT (AutoStartFlagChange(int)));
-  if(my_ros_->auto_start_flag_ == true){
-    ui_->checkBox_3->setCheckState(Qt::Checked);
-  }else{
-    ui_->checkBox_3->setCheckState(Qt::Unchecked);
-  }
+
+  connect(ui_->checkBox, SIGNAL(stateChanged(int)), this, SLOT(LoopFlagChange(int)));
+  ui_->checkBox->setCheckState(loop_flag_ ? Qt::Checked : Qt::Unchecked);
+
+  connect(ui_->checkBox_2, SIGNAL(stateChanged(int)), this, SLOT(StopSkipFlagChange(int)));
+  ui_->checkBox_2->setCheckState(stop_skip_flag_ ? Qt::Checked : Qt::Unchecked);
+
+  connect(ui_->checkBox_3, SIGNAL(stateChanged(int)), this, SLOT(AutoStartFlagChange(int)));
+  ui_->checkBox_3->setCheckState(my_ros_->auto_start_flag_ ? Qt::Checked : Qt::Unchecked);
 
   connect(ui_->horizontalSlider, SIGNAL(sliderPressed()), this, SLOT(SliderPressed()));
   connect(ui_->horizontalSlider, SIGNAL(valueChanged(int)), this, SLOT(SliderValueChange(int)));
   connect(ui_->horizontalSlider, SIGNAL(sliderReleased()), this, SLOT(SliderValueApply()));
 
-  ui_->horizontalSlider->setRange(0,10000);
+  ui_->horizontalSlider->setRange(0, 10000);
   ui_->horizontalSlider->setValue(0);
-  slider_value_ = 0;
-
 }
 
 MainWindow::~MainWindow()
 {
-  emit setThreadFinished(true); //Tell the thread to finish
+  emit setThreadFinished(true);
   delete ui_;
   my_ros_->quit();
-  if(!my_ros_->wait(500)) //Wait until it actually has terminated (max. 3 sec)
-  {
-      my_ros_->terminate(); //Thread didn't exit in time, probably deadlocked, terminate it!
-      my_ros_->wait(); //We have to wait again here!
+  if (!my_ros_->wait(500)) {
+    my_ros_->terminate();
+    my_ros_->wait();
   }
 }
 
-
-void MainWindow::RosInit(ros::NodeHandle &n)
+void MainWindow::RosInit(const rclcpp::Node::SharedPtr &node)
 {
-  my_ros_->ros_initialize(n);
+  node_ = node;
+  // TODO(ros2-migration): bước hiện tại chỉ chuyển Node initialization ở main/mainwindow.
+  // ROSThread vẫn còn ROS1 API và sẽ được chuyển ở bước tiếp theo.
 }
-
 
 void MainWindow::TryClose()
 {
   close();
 }
 
-
 void MainWindow::FilePathSet()
 {
   play_flag_ = false;
   my_ros_->play_flag_ = false;
-  this->ui_->pushButton_2->setText(QString::fromStdString("Play"));
+  ui_->pushButton_2->setText(QString::fromStdString("Play"));
 
   pause_flag_ = false;
   my_ros_->pause_flag_ = false;
-  this->ui_->pushButton_3->setText(QString::fromStdString("Pause"));
+  ui_->pushButton_3->setText(QString::fromStdString("Pause"));
 
   QFileDialog dialog;
-  this->ui_->label->setText("Data is beging loaded.....");
+  ui_->label->setText("Data is beging loaded.....");
   data_folder_path_ = dialog.getExistingDirectory();
   my_ros_->data_folder_path_ = data_folder_path_.toUtf8().constData();
 
   my_ros_->Ready();
-
-  this->ui_->label->setText(data_folder_path_);
-
+  ui_->label->setText(data_folder_path_);
 }
 
 void MainWindow::SetStamp(quint64 stamp)
 {
-  this->ui_->label_2->setText(QString::number(stamp));
-
-  //set slide bar
-  if(slider_checker_ == false){
-    ui_->horizontalSlider->setValue(static_cast<int>(static_cast<float>(stamp - my_ros_->initial_data_stamp_)/static_cast<float>(my_ros_->last_data_stamp_ - my_ros_->initial_data_stamp_)*10000));
+  ui_->label_2->setText(QString::number(stamp));
+  if (!slider_checker_) {
+    ui_->horizontalSlider->setValue(static_cast<int>(
+        static_cast<float>(stamp - my_ros_->initial_data_stamp_) /
+        static_cast<float>(my_ros_->last_data_stamp_ - my_ros_->initial_data_stamp_) * 10000));
   }
 }
 
 void MainWindow::Play()
 {
-  if(my_ros_->play_flag_ == false){
+  if (!my_ros_->play_flag_) {
     play_flag_ = true;
     my_ros_->play_flag_ = true;
-    this->ui_->pushButton_2->setText(QString::fromStdString("End"));
+    ui_->pushButton_2->setText(QString::fromStdString("End"));
 
     pause_flag_ = false;
     my_ros_->pause_flag_ = false;
-    this->ui_->pushButton_3->setText(QString::fromStdString("Pause"));
-  }else{
+    ui_->pushButton_3->setText(QString::fromStdString("Pause"));
+  } else {
     play_flag_ = false;
     my_ros_->play_flag_ = false;
-    this->ui_->pushButton_2->setText(QString::fromStdString("Play"));
+    ui_->pushButton_2->setText(QString::fromStdString("Play"));
   }
 }
 
 void MainWindow::Pause()
 {
-  if(pause_flag_ == false){
+  if (!pause_flag_) {
     pause_flag_ = true;
     my_ros_->pause_flag_ = true;
-    this->ui_->pushButton_3->setText(QString::fromStdString("Resume"));
-  }else{
+    ui_->pushButton_3->setText(QString::fromStdString("Resume"));
+  } else {
     pause_flag_ = false;
     my_ros_->pause_flag_ = false;
-    this->ui_->pushButton_3->setText(QString::fromStdString("Pause"));
+    ui_->pushButton_3->setText(QString::fromStdString("Pause"));
   }
 }
-
 
 void MainWindow::PlaySpeedChange(double value)
 {
@@ -152,32 +136,35 @@ void MainWindow::PlaySpeedChange(double value)
 
 void MainWindow::LoopFlagChange(int value)
 {
-  if(value == 2){
+  if (value == 2) {
     loop_flag_ = true;
     my_ros_->loop_flag_ = true;
-  }else if(value == 0){
+  } else if (value == 0) {
     loop_flag_ = false;
     my_ros_->loop_flag_ = false;
   }
 }
+
 void MainWindow::StopSkipFlagChange(int value)
 {
-  if(value == 2){
+  if (value == 2) {
     stop_skip_flag_ = true;
     my_ros_->stop_skip_flag_ = true;
-  }else if(value == 0){
+  } else if (value == 0) {
     stop_skip_flag_ = false;
     my_ros_->stop_skip_flag_ = false;
   }
 }
+
 void MainWindow::AutoStartFlagChange(int value)
 {
-  if(value == 2){
+  if (value == 2) {
     my_ros_->auto_start_flag_ = true;
-  }else if(value == 0){
+  } else if (value == 0) {
     my_ros_->auto_start_flag_ = false;
   }
 }
+
 void MainWindow::SliderValueChange(int value)
 {
   slider_value_ = value;
